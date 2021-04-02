1e002a3a3fe9579b097adadcbeb87878970d2235
Faith1sGay <daniel.viola001@mymdc.net>
2021-04-02 16:02:41 -0400

    practice part 3 the electric boogalee

---
+++ ReturnSomething/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReturnSomething
+{
+    class Program
+    {
+        static string ReturnSomething(string x)
+        {
+            return "Something " + x;
+        }
+        static void Main()
+        {
+           var x =  ReturnSomething(Console.ReadLine());
+            Console.WriteLine(x);
+        }
+    }
+}

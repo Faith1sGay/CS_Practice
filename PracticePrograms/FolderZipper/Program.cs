8e8cad819331f649fb4eeecf71ce51a781fad53e
Faith1sGay <daniel.viola001@mymdc.net>
2021-04-14 16:05:24 -0400

    i have to work more on a couple of these, i just can't muster enough energy to rn

---
+++ PracticePrograms/FolderZipper/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+namespace FolderZipper
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("== File Zipper ==");
+            Console.WriteLine("Please enter the number of the option you would like to do : \n1. Zip a File\n2. Extract a zip file.");
+            var Choice = int.Parse(Console.ReadLine());
+
+            switch (Choice)
+            {
+                case 1:
+                    Console.WriteLine("What is the start path?");
+                    var start = Console.ReadLine();
+                    var zip = Console.ReadLine();
+                    try
+                    {
+                        ZipFile.CreateFromDirectory(start, zip);
+                    }
+                    catch (FileNotFoundException e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("What is the zip file you would like extracted?");
+                    var extract = Console.ReadLine();
+                    var Zip = Console.ReadLine();
+                    try
+                    {
+                        ZipFile.ExtractToDirectory(Zip, extract);
+                    }
+                    catch (UnauthorizedAccessException _e)
+                    {
+                        Console.WriteLine(_e.ToString());
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ PracticePrograms/HypixelCLI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Hypixel.NET;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace HypixelCLI
+{
+    internal class ApiCreds
+    {
+        public string ApiKey
+        {
+            get; set;
+        }
+    }
+
+    class Program
+    {
+        public static ApiCreds creds = null;
+
+        static void Main()
+        {
+            creds = JsonSerializer.Deserialize<ApiCreds>(File.ReadAllText("../../../ApiCreds.json"));
+            HypixelApi api = new HypixelApi(creds.ApiKey, 300);
+            int opts;
+            Console.WriteLine("Please Enter MineCraft Username : ");
+            string user = Console.ReadLine();
+            Console.WriteLine("== HYPIXEL STAT CLI ==");
+            Console.WriteLine("Please enter the option for Hypixel Stats that you want: \n1.QuakeCraft\n2.Walls\n3.Paintball\n4.Survival Games\n5.TNT Games\n6.VampireZ");
+            opts = int.Parse(Console.ReadLine());
+            switch (opts)
+            {
+                case 1:
+                    var qc = api.GetUserByPlayerName(user);
+                    Console.WriteLine(qc.Player.LastLogin);
+                    break;
+            }
+        }
+    }
+}

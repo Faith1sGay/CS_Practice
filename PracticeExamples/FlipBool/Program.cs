3694e9691f6763d91880cdb5a29344f7335b01fd
Faith1sGay <daniel.viola001@mymdc.net>
2021-06-07 11:36:29 -0400

    yeet

---
+++ PracticeExamples/FlipBool/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FlipBool
+{
+    internal class Program
+    {
+        internal static bool Flip(bool x)
+        {
+            if (x == true) return false;
+            else return true;
+        }
+
+        private static void Main()
+        {
+            Console.WriteLine("What is the boolean you would like to be flipped?");
+            var a = Console.ReadLine();
+            bool b;
+            var c = bool.TryParse(a, out b);
+            Console.WriteLine(Flip(b));
+        }
+    }
+}

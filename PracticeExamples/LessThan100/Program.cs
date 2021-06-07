2faeb32aae62032cf35cb53b1b44fb9f965e6d5a
Faith1sGay <daniel.viola001@mymdc.net>
2021-06-07 11:14:14 -0400

    Moar practice

---
+++ PracticeExamples/LessThan100/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LessThan100
+{
+    static class Program
+    {
+        internal static bool LessThan100(int x, int y)
+        {
+            if (x + y < 100) return true;
+            else return false;
+        }
+        internal static void Main()
+        {
+            Console.Write("What is your first number? ");
+            var a = int.Parse(Console.ReadLine());
+            Console.Write("What is your second number? ");
+            var b = int.Parse(Console.ReadLine());
+            Console.Write($"Is {a} + {b} larger or smaller than 100?\n{LessThan100(a, b)}");
+        }
+    }
+}

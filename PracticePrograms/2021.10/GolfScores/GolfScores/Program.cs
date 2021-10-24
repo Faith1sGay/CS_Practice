8c3f3412338c1729d9f8be46fbf4c1ce6d06760a
Daniel Viola <daniel.viola001@mymdc.net>
2021-10-24 12:27:37 -0400

    Been a while since I've worked on this lol

---
+++ PracticePrograms/2021.10/GolfScores/GolfScores/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GolfScores
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Please enter the par for the golf hole: ");
+            int.TryParse(Console.ReadLine(), out int par);
+            Console.WriteLine("Now please enter the amount of shots taken for that hole: ");
+            int.TryParse(Console.ReadLine(), out int shots);
+            var eagle = par - 2;
+            var birdie = par - 1;
+            var bogey = par + 1;
+            if (par is 3 or 4 or 5)
+            {
+                if (shots == par)
+                {
+                    Console.WriteLine("Par");
+                }
+                else if (shots == eagle)
+                {
+                    Console.WriteLine("Eagle");
+                }
+                else if (shots == birdie)
+                {
+                    Console.WriteLine("Birdie");
+                }
+                else if (shots == bogey)
+                {
+                    Console.WriteLine("Bogey");
+                }
+            }
+            else
+            {
+                throw new Exception("The par must be 3 or 4 or 5 and the shots must be either up to 2 below par or one above par.");
+            }
+        }
+    }
+}

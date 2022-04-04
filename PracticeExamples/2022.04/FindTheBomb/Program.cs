e38a7780eb26335b3c391403eee9f9d447365a9e
Daniel Viola <daniel.viola001@mymdc.net>
2022-04-04 19:17:23 -0400

    A

---
+++ PracticeExamples/2022.04/FindTheBomb/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FindTheBomb
+{
+    internal class Program
+    {
+        private static string Bomb(string x)
+        {
+            if (x.Contains("bomb".ToLower()))
+            {
+                return "Duck!!!";
+            }
+            else
+            {
+                return "There is no bomb, relax.";
+            }
+        }
+
+        private static void Main() => Console.WriteLine(Bomb(Console.ReadLine()));
+    }
+}

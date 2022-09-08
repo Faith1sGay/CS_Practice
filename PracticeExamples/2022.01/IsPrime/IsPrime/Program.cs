38450a056916be9ad5d8d35a1eef02bd62a1b39b
Daniel Viola <daniel.viola001@mymdc.net>
2022-09-08 12:05:49 -0400

    first time making a program in forever lol

---
+++ PracticeExamples/2022.01/IsPrime/IsPrime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace IsPrime
+{
+    static internal class Program
+    {
+        static bool IsPrimeNumber(int num)
+        {
+            if (num % 2 == 0) return false;
+            else return true;
+        }
+        private static void Main()
+        {
+            Console.WriteLine("Enter a number to check to see if it's prime or not : ");
+            int.TryParse(Console.ReadLine(), out var number);
+            Console.WriteLine(IsPrimeNumber(number));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PracticePrograms/2022.09/StringRepeater/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringRepeater
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("What is the string you would like to be repeated?");
+            Console.WriteLine(StringTimes(Console.ReadLine(), 4000));
+        }
+
+        private static string StringTimes(string s, uint n)
+        {
+            string res = "";
+            for (int i = 0; i < n; i++)
+            {
+                res += s;
+            }
+            return res;
+        }
+    }
+}

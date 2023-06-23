5581adb279b4cb98aea3ce9cadd22c64b8c00c39
Daniel Viola <daniel.viola001@mymdc.net>
2023-06-23 15:23:58 -0400

    a

---
+++ PracticePrograms/2023.06/NextLicensePlateNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace NextLicensePlateNumber
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Write("Enter the current license plate number: ");
+            string? currentPlate = Console.ReadLine();
+            string? nextPlate = GenerateNextLicensePlateNumber(currentPlate);
+            Console.WriteLine("Next license plate number: " + nextPlate);
+        }
+
+        private static string? GenerateNextLicensePlateNumber(string? currentPlateNumber)
+        {
+            if (string.IsNullOrEmpty(currentPlateNumber))
+            {
+                return "AAA000";
+            }
+            char[] nextPlate = currentPlateNumber.ToCharArray();
+            bool carry = true;
+            for (int i = nextPlate.Length - 1; i >= 0; i--)
+            {
+                if (carry)
+                {
+                    if (nextPlate[i] == '9')
+                    {
+                        nextPlate[i] = '0';
+                    }
+                    else if (nextPlate[i] == 'Z')
+                    {
+                        nextPlate[i] = 'A';
+                    }
+                    else
+                    {
+                        nextPlate[i]++;
+                        carry = false;
+                    }
+                }
+            }
+            if (carry)
+            {
+                return "AAA000";
+            }
+            return new string(nextPlate);
+        }
+    }
+}
